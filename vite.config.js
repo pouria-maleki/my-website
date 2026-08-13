7f3900f69f95ed1f9fc2302477e88eeb166283c8
pouria maleki <61584820+pouria-maleki@users.noreply.github.com>
2026-08-13 21:59:14 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: { sourcemap: true }
+  build: {
+    sourcemap: true
+  },
+
+  // Fix SSR / prerender compatibility with react-helmet-async
+  ssr: {
+    noExternal: ['react-helmet-async']
+  }
 })
